efca5218d48810205839537475f72fe73ecf7c93
Oscar-M10 <oscar_medellin18@outlook.es>
2022-06-16 13:09:31 -0600

    01-fundamentos: alerts, apps, primitivos

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,7 @@
+//alert('Hola Mundo');
+
+let nombre = prompt('¿Cuál es tu nombre?', 'sin nombre');
+console.log('****' + nombre + '****');
+
+
+const seleccion = confirm('¿Estas seguro de borrar esto?')
---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,34 @@
+
+
+//console.log('Hola Mundo');
+
+//let a = 10;
+//var b = 10;
+//const c = 10;
+//Esto no se puede hacer
+//c = 20;
+//a = 20;
+//b = 30;
+
+
+//INTRODUCCIÓN A LA CONSOLA
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a +b;
+
+const saludo = c + d; 
+
+//console.log('%c Mis variables','color:blue; fount-weight: bold');
+//console.log({a});
+//console.log({b});
+//console.log({c});
+c = 'HOLA DE NUEVO';
+//console.table({a, b, c, d, x});
+//alert('hola desde apps.js');
+
+var miNombre = 'oscar';
+
+var outerWidth = 1000000; 
