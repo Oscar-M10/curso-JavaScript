ba8d590a50ce3ced7cde88dc4e39cda1f9e23e3d
Oscar-M10 <oscar_medellin18@outlook.es>
2022-06-16 17:24:41 -0600

    01-fundamentos: arreglos-palabras nuevo

---
+++ 01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,19 @@
+let videoJuegos = ['Mario3','Megaman','Chrono Trigger'];
+console.log({videoJuegos});
+
+console.log(videoJuegos[0]);
+//Creando arreglos 
+let arregloCosas = [
+    true,
+    123,
+    'Oscar',
+    1 + 2,
+    //fuction(){},
+    ()=>{},
+    {a:1},
+    ['X', 'Megaman', 'Zero', 'Dr. Light',[
+        'Dr. Wily',
+        'Woodman'
+    ]],
+
+];
---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,29 @@
+let nombre = 'Peter Parker';
+console.log(nombre)
+
+let nombre2 = 'Peter Parker';
+console.log(nombre)
+nombre2 = 'Ben Parker'
+
+nombre = "Tía Mary";
+nombre = `Tía Mary`;
+
+console.log( typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+let esMarvel = false;
+console.log(typeof esMarvel);
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+
+let symbol2 = Symbol('a');
+
+console.log(typeof symbol1 === symbol2);
