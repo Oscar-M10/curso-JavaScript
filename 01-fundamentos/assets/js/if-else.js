34a784b7309cfdbf8aadbd90e7743bc2503908fc
Oscar-M10 <oscar_medellin18@outllok.es>
2022-12-02 09:23:59 -0700

    fundamentos

---
+++ 01-fundamentos/assets/js/if-else.js
@@ -0,0 +1,14 @@
+let a = 5;
+
+
+if (a >= 10) {
+    console.log('A es mayor o igual a 10');
+}else{
+    console.log('A es menor a 10');
+}
+
+//console.log('Fin de programa');
+
+const hoy = new Date();
+
+console.log(hoy);
